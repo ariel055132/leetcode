21dbe74e62ee3545d04294efeb43ec493d0f1e01
Adrian Li <ariel055132@gmail.com>
2025-08-28 07:13:12 +0800

    Enhance Q153 class to handle edge cases in finding the minimum element in a rotated sorted array

--- src/main/java/BinarySearch/Q153.java
+++ src/main/java/BinarySearch/Q153.java
@@ -2,16 +2,21 @@ package BinarySearch;
 
 public class Q153 {
     public int findMin(int[] nums) {
+        // Edge Case 1: If nums is empty, no minimum value can be found, therefore return -1
         if (nums == null) {
             return -1;
         }
+        // Edge Case 2: If the first element of array is less than the last element of array, means the array is not rotated
+        // Therefore, return the first element
         if (nums[0] <= nums[nums.length - 1]) {
             return nums[0];
         }
+        // Use Binary Search Approach to find the minimum element
         int left = 0;
         int right = nums.length - 1;
         while (left <= right) {
             int mid = left + (right - left) / 2;
+            // When
             if (nums[mid] >= nums[0]) {
                 left = mid + 1;
             } else {
