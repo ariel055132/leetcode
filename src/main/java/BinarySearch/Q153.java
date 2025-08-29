a4685193afe84dd237d2ff967edf2e614d1c6a2a
Adrian Li <ariel055132@gmail.com>
2025-08-29 08:22:45 +0800

    Refactor Q153 class to remove commented-out code for improved readability

--- src/main/java/BinarySearch/Q153.java
+++ src/main/java/BinarySearch/Q153.java
@@ -16,7 +16,6 @@ public class Q153 {
         int right = nums.length - 1;
         while (left <= right) {
             int mid = left + (right - left) / 2;
-            // When
             if (nums[mid] >= nums[0]) {
                 left = mid + 1;
             } else {
