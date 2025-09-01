a477e70452d2bea3efff015ae7510c96f25218b4
Adrian Li <ariel055132@gmail.com>
2025-09-02 07:22:10 +0800

    Add Q167 class to implement two-sum problem with corresponding tests

---
+++ src/test/java/Q167_test.java
@@ -0,0 +1,16 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class Q167_test {
+    @Test
+    @DisplayName("Example 1")
+    void test1() {
+        int[] numbers = {2, 7, 11, 15};
+        int target = 9;
+        Q167 q167 = new Q167();
+        int[] expectResult = {1, 2};
+        int[] actualResult = q167.twoSum(numbers, target);
+        Assertions.assertArrayEquals(expectResult, actualResult);
+    }
+}
