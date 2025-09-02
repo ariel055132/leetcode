d71ec2b95772b3babd9ee6d11a886ff376c8cb91
Adrian Li <ariel055132@gmail.com>
2025-09-02 23:15:58 +0800

    Add Q713 class to count subarrays with product less than k and corresponding tests

---
+++ src/main/java/Q713.java
@@ -0,0 +1,17 @@
+public class Q713 {
+    public int numSubarrayProductLessThanK(int[] nums, int k) {
+        int n = nums.length;
+        int product = 1; // otherwise always become 0
+        int left = 0;
+        int ans = 0;
+        for (int right = 0; right < n; right++) {
+            product *= nums[right];
+            while (product >= k && left <= right) {
+                product /= nums[left];
+                left++;
+            }
+            ans += (right - left + 1);
+        }
+        return ans;
+    }
+}
---
+++ src/test/java/Q713_test.java
@@ -0,0 +1,27 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class Q713_test {
+    @Test
+    @DisplayName("Example 1")
+    void test1() {
+        Q713 q713 = new Q713();
+        int[] nums = {10, 5, 2, 6};
+        int k = 100;
+        int actualResult = q713.numSubarrayProductLessThanK(nums, k);
+        int expectResult = 8;
+        Assertions.assertEquals(expectResult, actualResult);
+    }
+
+    @Test
+    @DisplayName("Example 2")
+    void test2() {
+        Q713 q713 = new Q713();
+        int[] nums = {1, 2, 3};
+        int k = 0;
+        int actualResult = q713.numSubarrayProductLessThanK(nums, k);
+        int expectResult = 0;
+        Assertions.assertEquals(expectResult, actualResult);
+    }
+}
