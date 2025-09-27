0ccd8f6a69f253708e4eae3bd749917e63be474e
Adrian Li <ariel055132@gmail.com>
2025-09-28 00:46:48 +0800

    Add Q611 class to count the number of valid triangle triplets from an integer array and corresponding documentation

--- src/main/java/Q611.java
+++ src/main/java/Q611.java
@@ -1,7 +1,17 @@
 import java.util.Arrays;
 
 public class Q611 {
+    /**
+     * Given an integer nums, return the number of triplets chosen from the array that can make triangles if we take them as side lengths of a triangle
+     *
+     * Definition of a triangle
+     * Let s1, s2, s3 be sides of triangle, and s1 < s2 < s3
+     * If the three sides can form a triangle, it should be s1 + s2 > s3
+     * @param nums integer array
+     * @return result integer (number of triplets chosen from the array that can make triangles if we take them as side lengths of a triangle)
+     */
     public int triangleNumber(int[] nums) {
+        // Sort the array
         Arrays.sort(nums);
         int result = 0;
         for (int i = 2; i < nums.length; i++) {
