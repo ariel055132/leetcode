1ec52844c31a97d89e4f2e4188e29eb6050b6f11
ariel055132 <36882230+ariel055132@users.noreply.github.com>
2025-11-30 16:26:55 +0800

    Add Q1512_HashMap class to count good pairs in an array and include corresponding unit tests

---
+++ src/main/java/Q1512_HashMap.java
@@ -0,0 +1,27 @@
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Q1512_HashMap {
+    /**
+     * Given an array of integer nums, return the number of good pairs.
+     * A pair (i, j) is called good if nums[i] == nums[j] and i < j.
+     *
+     * @param nums
+     * @return
+     */
+    public int numIdenticalPairs(int[] nums) {
+        int result = 0;
+        Arrays.sort(nums);
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int num : nums) {
+            if (map.get(num) == null) {
+                map.put(num, 1);
+            } else {
+                map.put(num, map.get(num) + 1);
+            }
+            result += map.get(num) - 1;
+        }
+        return result;
+    }
+}
---
+++ src/test/java/Q1512_test.java
@@ -0,0 +1,25 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class Q1512_test {
+    @Test
+    @DisplayName("Test 1 Normal Case (HashMap)")
+    void test1() {
+        Q1512_HashMap q1512 = new Q1512_HashMap();
+        int[] nums = {1, 2, 3, 1, 1, 3};
+        int expectResult = 4;
+        int actualResult = q1512.numIdenticalPairs(nums);
+        Assertions.assertEquals(expectResult, actualResult);
+    }
+
+    @Test
+    @DisplayName("Test 2 All good Pairs")
+    void test2() {
+        Q1512_HashMap q1512 = new Q1512_HashMap();
+        int[] nums = {1, 1, 1, 1};
+        int expectResult = 6;
+        int actualResult = q1512.numIdenticalPairs(nums);
+        Assertions.assertEquals(expectResult, actualResult);
+    }
+}
