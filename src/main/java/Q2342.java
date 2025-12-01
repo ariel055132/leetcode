7af306b210718c180e1f06ebfd3c021e9bd6eac7
ariel055132 <36882230+ariel055132@users.noreply.github.com>
2025-12-01 23:14:24 +0800

    Add Q2342 class to calculate the maximum sum of pairs with the same digit sum and include corresponding unit tests

---
+++ src/main/java/Q2342.java
@@ -0,0 +1,31 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class Q2342 {
+    public int maximumSum(int[] nums) {
+        // 1. Set up result for return, default MIN_VALUE for update
+        int result = Integer.MIN_VALUE;
+        // 2. Create a map
+        // Key: Sum of digits of the number; Value: Sum of number with same digits
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int num : nums) {
+            int key = 0;
+            int tmpNum = num;
+            // 3. Find the sum of digits of the number with while loop
+            while (tmpNum != 0) {
+                key += tmpNum % 10;
+                tmpNum /= 10;
+            }
+            //
+            if (!map.containsKey(key)) {
+                map.put(key, num);
+            } else {
+                int previousMax = map.get(key);
+                int currentValue = previousMax + num;
+                result = Math.max(result, currentValue);
+                map.put(key, Math.max(num, previousMax));
+            }
+        }
+        return result == Integer.MIN_VALUE ? -1 : result;
+    }
+}
---
+++ src/test/java/Q2342_test.java
@@ -0,0 +1,35 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class Q2342_test {
+    @Test
+    @DisplayName("Test 1")
+    void test1() {
+        Q2342 q2342 = new Q2342();
+        int[] nums = {18, 43, 36, 13, 7};
+        int expectResult = 54;
+        int actualResult = q2342.maximumSum(nums);
+        Assertions.assertEquals(expectResult, actualResult);
+    }
+
+    @Test
+    @DisplayName("Test 2")
+    void test2() {
+        Q2342 q2342 = new Q2342();
+        int[] nums = {10, 12, 19, 14};
+        int expectResult = -1;
+        int actualResult = q2342.maximumSum(nums);
+        Assertions.assertEquals(expectResult, actualResult);
+    }
+
+    @Test
+    @DisplayName("Test 3")
+    void test3() {
+        Q2342 q2342 = new Q2342();
+        int[] nums = {368, 369, 307, 304, 384, 138, 90, 279, 35, 396, 114, 328, 251, 364, 300, 191, 438, 467, 183};
+        int expectResult = 835;
+        int actualResult = q2342.maximumSum(nums);
+        Assertions.assertEquals(expectResult, actualResult);
+    }
+}
