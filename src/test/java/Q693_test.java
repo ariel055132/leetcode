e5374226c9aec9006b4aefb392106a1b958b664d
Adrian Li <ariel055132@gmail.com>
2026-02-18 14:52:58 +0800

    Refactor Q693_test class to improve test case naming and add comments for clarity

--- src/test/java/Q693_test.java
+++ src/test/java/Q693_test.java
@@ -4,11 +4,31 @@ import org.junit.jupiter.api.Test;
 
 public class Q693_test {
     @Test
-    @DisplayName("Test Case 1")
+    @DisplayName("Test 1")
     void test1() {
         Q693 q693 = new Q693();
-        int n = 5;
+        int n = 5; // 101
         boolean expectResult = true;
+        boolean actualResult = q693.hasAlternatingBits(n);
+        Assertions.assertEquals(expectResult, actualResult);
+    }
+
+    @Test
+    @DisplayName("Test 2")
+    void test2() {
+        Q693 q693 = new Q693();
+        int n = 7; // 111
+        boolean expectResult = false;
+        boolean actualResult = q693.hasAlternatingBits(n);
+        Assertions.assertEquals(expectResult, actualResult);
+    }
+
+    @Test
+    @DisplayName("Test 3")
+    void test3() {
+        Q693 q693 = new Q693();
+        int n = 11;
+        boolean expectResult = false;
         boolean actualResult = q693.hasAlternatingBits(n);
         Assertions.assertEquals(expectResult, actualResult);
     }
