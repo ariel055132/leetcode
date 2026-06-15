5ddeb00b4064c75b969834f20a9dbbadc0778fce
ariel055132 <ariel055132@gmail.com>
2026-06-16 00:07:21 +0800

    Add Q2095 class documentation and additional test cases for deleteMiddle method

--- src/main/java/LinkedList/Q2095.java
+++ src/main/java/LinkedList/Q2095.java
@@ -2,6 +2,16 @@ package LinkedList;
 
 public class Q2095 {
     /**
+     * You are given the head of a linked list. Delete the middle node, and return the head of the modified linked list.
+     * The middle node of a linked list of size n is the ⌊n / 2⌋th node from the start using 0-based indexing, where ⌊x⌋ denotes the largest integer less than or equal to x.
+     * For n = 1, 2, 3, 4, and 5, the middle nodes are 0, 1, 1, 2, and 2, respectively.
+     * 
+     * Algorithm:
+     * Use slow and fast pointers and traverse the array.
+     * When fast pointer reaches the end, slow is at the middle node. (Just skip slow)
+     * Therefore, also need a pointer to the node before slow, so that we can skip the middle node
+     * 
+     * Example as follows:
      * 1 -> 2 -> 3 -> 4
      * (-100) -> 1 -> 2 -> 3 -> 4
      * 1 (slow) -> 2 (fast) -> 3 -> 4
--- src/test/java/LinkedList/Q2095_test.java
+++ src/test/java/LinkedList/Q2095_test.java
@@ -13,6 +13,15 @@ public class Q2095_test extends BaseLinkedListTest {
         System.out.println(areLinkedListsEqual(expectResult, actualResult));
     }
 
+    @Test
+    @DisplayName("Test 2")
+    void test2() {
+        Q2095 q2095 = new Q2095();
+        ListNode expectResult = expectResult2();
+        ListNode actualResult = q2095.deleteMiddle(list2());
+        System.out.println(areLinkedListsEqual(expectResult, actualResult));
+    }
+
     ListNode list1() {
         ListNode head = new ListNode(1);
         ListNode node1 = new ListNode(2);
@@ -30,6 +39,38 @@ public class Q2095_test extends BaseLinkedListTest {
         ListNode node2 = new ListNode(4);
         head.next = node1;
         node1.next = node2;
+        return head;
+    }
+
+    ListNode list2() {
+        ListNode node1 = new ListNode(1);
+        ListNode node2 = new ListNode(3);
+        ListNode node3 = new ListNode(4);
+        ListNode node4 = new ListNode(7);
+        ListNode node5 = new ListNode(1);
+        ListNode node6 = new ListNode(2);
+        ListNode node7 = new ListNode(6);
+        node1.next = node2;
+        node2.next = node3;
+        node3.next = node4;
+        node4.next = node5;
+        node5.next = node6;
+        node6.next = node7;
+        return node1;
+    }
+
+    ListNode expectResult2() {
+        ListNode head = new ListNode(1);
+        ListNode node1 = new ListNode(3);
+        ListNode node2 = new ListNode(4);
+        ListNode node3 = new ListNode(1);
+        ListNode node4 = new ListNode(2);
+        ListNode node5 = new ListNode(6);
+        head.next = node1;
+        node1.next = node2;
+        node2.next = node3;
+        node3.next = node4;
+        node4.next = node5;
         return head;
     }
 }
