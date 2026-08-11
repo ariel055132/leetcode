0c7f4b0eaaf0ed896fb4d9c2f0d3893de607263c
ariel055132 <ariel055132@gmail.com>
2026-08-11 22:56:10 +0800

    Add Q1749 class with maxAbsoluteSum method to calculate maximum absolute sum of subarrays

---
+++ src/main/java/PrefixSum/Q1749.java
@@ -0,0 +1,29 @@
+package PrefixSum;
+
+public class Q1749 {
+    /**
+     * You are given an integer array nums. The absolute sum of a subarray [numsl, numsl+1, ..., numsr-1, numsr] is abs(numsl + numsl+1 + ... + numsr-1 + numsr).
+     * Return the maximum absolute sum of any (possibly empty) subarray of nums.
+     * Note that abs(x) is defined as follows:
+     * If x is a negative integer, then abs(x) = -x
+     * If x is a non-negative integer, then abs(x) = x.
+     * 
+     * Brute Force approach: emumerates everu possible subarray.
+     * For each starting index, extend the ending index while maintaining a running sum
+     * 
+     * @param nums
+     * @return
+     */
+    public int maxAbsoluteSum(int[] nums) {
+        int result = 0;
+        for (int i = 0; i < nums.length; i++) {
+            int currentSum = 0;
+            for (int j = i; j < nums.length; j++) {
+                currentSum += nums[j];
+                // currentSum need to abs with the instruction of questions
+                result = Math.max(Math.abs(currentSum), result); 
+            }
+        }
+        return result;
+    }
+}
